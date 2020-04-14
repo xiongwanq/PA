c540e8c8856bc6888fc7e309fccf47a3a4abb62d
tracer-ics2017 <tracer@njuics.org>
2020-04-14 21:16:41 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:16:41 up  5:44,  2 users,  load average: 0.00, 0.00, 0.00 da841245458055683562b1a0afda58fa19a21f8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -271,13 +271,8 @@ uint32_t eval(int p, int q) {
 		case '-': return val1 - val2;
 		case '*': return val1 * val2;
 		case '/': 
-			if(val2 == 0){
-				printf("cannot devide 0!");
-				return -1;
-				//assert(0);
-			}
-			else
-				return val1 / val2;
+			Assert(val2 == 0, "cannot devide 0!");
+			return val1 / val2;
 		case TK_EQ: return val1 == val2;
 		case TK_UEQ: return val1 != val2;
 		case '&': return val1 && val2;
