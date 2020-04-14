de650814c812ce1542de30a9b5b96170e0092e9e
tracer-ics2017 <tracer@njuics.org>
2020-04-14 18:33:41 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  18:33:41 up  3:12,  1 user,  load average: 0.01, 0.01, 0.00 37c3be023e6d795878a34c021b7a9e1ce51c6b49

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -231,7 +231,7 @@ uint32_t eval(int p, int q) {
 	}
     else if(tokens[p].type == TK_REG){
 		printf("reg!233!\n");
-		char *reg = "$";
+		char reg[20] = "$";
 		printf("reg=%s\n",reg);
 		strcat(reg,tokens[p].str);
 		printf("reg=%s\n",reg);
