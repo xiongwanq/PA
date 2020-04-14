b5c762088e1a93e1be9c0e12550f674124092f96
tracer-ics2017 <tracer@njuics.org>
2020-04-14 18:23:05 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  18:23:05 up  3:01,  1 user,  load average: 0.00, 0.00, 0.00 454b5e393808f55f9f2b99427d59aa135c6cda06

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -116,7 +116,8 @@ static bool make_token(char *e) {
         break;
       }
     }
-
+	for(int j = 0;j < nr_token;j++)
+		printf("%s", tokens[j].str);
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
