d32853388d00b0e8f8bcc0b171ad36c6f66481cd
tracer-ics2017 <tracer@njuics.org>
2020-05-02 19:55:57 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  19:55:57 up  1:45,  1 user,  load average: 0.09, 0.12, 0.11 451449b6c32e140e7088dfc162e855fefc063631

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -14,10 +14,10 @@ make_EHelper(and) {
 
 make_EHelper(xor) {
   printf("dest:%x, src:%x, src2:%x\n",id_dest->val, id_src->val, id_src2->val);
-  rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
-  operand_write(id_dest, &id_dest->val);
-
   rtlreg_t t;
+  rtl_xor(&t, &id_src->val, &id_src2->val);
+  operand_write(id_dest, &t);
+
   t = 0;
   rtl_set_OF(&t);
   rtl_set_CF(&t);
