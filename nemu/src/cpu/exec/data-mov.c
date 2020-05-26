50a466b71363dc66d4ddd4bf306c710b4e4b36b9
tracer-ics2017 <tracer@njuics.org>
2020-05-26 17:03:43 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  17:03:43 up  4:02,  1 user,  load average: 0.20, 0.21, 0.18 e6c0695c45f03485ad366a38eb5b59be94511af5

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -55,14 +55,18 @@ make_EHelper(cltd) {
   }
   else {
 	rtl_lr_l(&t, R_EAX);
-  printf("R_EAX=0x%x\n",t);
+    printf("R_EAX=0x%x\n",t);
     if(t < 0){
 	  t_val = 0xffffffff;
 	  rtl_sr_l(R_EDX, &t_val);
 	}
 	else{
+	  printf("----before-----\n");
+	  printf("R_EDX=0x%x\n",reg_l(R_EDX));
 	  t_val = 0;
 	  rtl_sr_w(R_EDX, &t_val);
+	  printf("----after-----\n");
+	  printf("t_val=0x%x,R_EDX=0x%x\n",t_val,reg_l(R_EDX));
 	}
   }
 
