f147c3bd1c39785e12cbd732dfefcd1e5f1188c5
tracer-ics2017 <tracer@njuics.org>
2020-06-06 10:09:23 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  10:09:23 up 8 min,  1 user,  load average: 0.16, 0.13, 0.05 c58ea04ccbb5069dfa027f4b28a817abab3a8ee

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,18 +5,23 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  vaddr_t gate_addr = cpu.idtr.base + NO * 8;
+
+  if(NO > cpu.idtr.limit){
+    assert(0);
+  }
 
   rtl_push(&cpu.eflags.value);
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
   
-  vaddr_t gate_addr = cpu.idtr.base + NO * 8;
   uint32_t low, high, offset;
   low = vaddr_read(gate_addr, 4) & 0x0000ffff;
   high = vaddr_read(gate_addr + 4, 4) & 0xffff0000;
   offset = low | high;
   decoding.is_jmp = true;
   decoding.jmp_eip = offset;
+
 }
 
 void dev_raise_intr() {
