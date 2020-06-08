8a6ac923362b84a51b6b059aa2a8db80c283a4e8
tracer-ics2017 <tracer@njuics.org>
2020-06-08 21:37:13 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:37:13 up 20:10,  1 user,  load average: 0.00, 0.04, 0.03 305e179c12b8b697bb54bf1ec2c4ae987da90fb0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -49,9 +49,9 @@ static inline uintptr_t sys_exit(_RegSet *r) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG1(r);
-  a[2] = SYSCALL_ARG1(r);
-  a[3] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
   switch (a[0]) {
     case SYS_none:
 	  sys_none(r);
