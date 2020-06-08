67e67174247ae13adc46480269790f862d8c57b7
tracer-ics2017 <tracer@njuics.org>
2020-06-08 21:27:14 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:27:14 up 20:00,  1 user,  load average: 0.00, 0.00, 0.00 7539944dad0f7ed28abec8244d0447732a856541

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,11 +1,10 @@
-//#include <unistd.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main() {
-//  write(1, "Hello World!\n", 13);
+  write(1, "Hello World!\n", 13);
   int i = 2;
-  //volatile 
-  int j = 0;
+  volatile int j = 0;
   while (1) {
     j ++;
     printf("j = %d\n",j);
