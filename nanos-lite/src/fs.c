d5907b5e949bd9bef7d8933a9686fee3638b7d58
tracer-ics2017 <tracer@njuics.org>
2020-06-08 15:22:22 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:22:22 up 16:15,  1 user,  load average: 0.13, 0.19, 0.30 2828f2d345bde197b07f31c989cbd34ee9c5aca

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -75,7 +75,7 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
   Finfo *f = file_table + fd;
   int remain_bytes = f->size - f->open_offset;
   int bytes_to_write = (remain_bytes > len ? len : remain_bytes);
-  Log("fd = 0x%x\n",fd);
+//  Log("fd = 0x%x\n",fd);
   switch (fd) {
     case FD_STDOUT:
     case FD_STDERR:
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 int main() {
+  printf("beforeWrite\n");
   write(1, "Hello World!\n", 13);
+  printf("afterWrite\n");
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
-//    printf("j = %d\n",j);
+    printf("j = %d\n",j);
     if (j == 10000) {
       printf("Hello World for the %dth time\n", i ++);
       j = 0;
