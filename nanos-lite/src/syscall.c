2e279f3770a2dcb533a723d7f625f7e347e332b3
tracer-ics2017 <tracer@njuics.org>
2020-06-09 13:09:37 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  13:09:37 up  1:38,  1 user,  load average: 0.79, 0.96, 0.61 ed42c22e1f0dd941e2be55d4fbe2e7629b39bba

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -63,7 +63,7 @@ _RegSet* do_syscall(_RegSet *r) {
 //	  break;
 	case SYS_write:
 	  Log("use write\n");
-	  eax = sys_write(a[1], a[2], a[3]);
+	  eax = sys_write(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
 	  break;
 	case SYS_brk:
 	  Log("use brk\n");
