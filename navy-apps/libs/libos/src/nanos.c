71c4c9c537a03c66dc04c36e78f29d8a5fc73262
tracer-ics2017 <tracer@njuics.org>
2020-06-09 20:06:07 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  20:06:07 up  8:34,  2 users,  load average: 0.10, 0.11, 0.09 34f77356621f62d91186ac32ec7a1cd5434259b

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -33,9 +33,11 @@ void *_sbrk(intptr_t increment){
   extern char end; /* Defined by the linker */
   static intptr_t program_break = (intptr_t)&end;
   intptr_t prev_program_break = program_break;
-  Log();  
+//  char str[80]; 
+//  sprintf(str, "Pi 的值 = %f", 3);  
+  write(1, "xxxxx yyyyy!\n", 13);
   if(_syscall_(SYS_brk, program_break + increment, 0, 0) == 0){
-    Log();
+    
 	program_break = prev_program_break + increment;
 	return (void *)prev_program_break;
   }
