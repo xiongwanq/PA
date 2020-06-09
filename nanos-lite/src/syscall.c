e288fe707ab91f557a6f635e6b3b96462e12c696
tracer-ics2017 <tracer@njuics.org>
2020-06-09 12:28:12 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  12:28:12 up 57 min,  1 user,  load average: 0.34, 0.34, 0.31 56a6b5f9edad1ffabaa08f8f314f9ea87ed922a3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,7 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-//  Log();
+  Log();
   return fs_write(fd, (void *)buf, len);
 }
 
@@ -29,7 +29,7 @@ static inline uintptr_t sys_close(uintptr_t fd) {
 }
 
 static inline uintptr_t sys_brk() {
-  Log();
+//  Log();
   return 0;
 }
 
