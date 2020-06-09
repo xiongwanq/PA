17badd4557dfd958883a114694ff230562429047
tracer-ics2017 <tracer@njuics.org>
2020-06-09 15:09:15 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:09:15 up  3:38,  1 user,  load average: 0.33, 0.26, 0.24 ea4e11cd3afeb1c41b15f9d4cbb42ffda7ba294f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,8 +5,7 @@
 int mm_brk(uint32_t new_brk);
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-//  SYSCALL_ARG1(r) = fs_open((char *)pathname, flags, mode);
-  return 1;
+  return fs_open((char *)pathname, flags, mode);
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -15,8 +14,7 @@ static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-//  SYSCALL_ARG1(r) = fs_read(fd, (void *)buf, len);
-  return 1;
+  return fs_read(fd, (void *)buf, len);
 }
 
 static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
@@ -24,8 +22,7 @@ static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t when
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-//  SYSCALL_ARG1(r) = fs_close(fd);
-  return 1;
+  return fs_close(fd);
 }
 
 static inline uintptr_t sys_brk() {
@@ -34,7 +31,7 @@ static inline uintptr_t sys_brk() {
 }
 
 static inline uintptr_t sys_none(_RegSet *r) {
-  SYSCALL_ARG1(r) = 1;
+//  SYSCALL_ARG1(r) = 1;
   return 1;
 }
   
@@ -52,7 +49,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
   switch (a[0]) {
     case SYS_none:
-	  sys_none(r);
+	  eax = sys_none(r);
 	  break;
     case SYS_exit:
 	  sys_exit(r);
@@ -61,26 +58,26 @@ _RegSet* do_syscall(_RegSet *r) {
 	  Log("use brk\n");
 	  eax = sys_brk();
 	  break;
-//	case SYS_open:
-//	  sys_open(r);
-//	  break;
+	case SYS_open:
+	  eax = sys_open(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
 	case SYS_write:
 	  Log("use write\n");
 	  eax = sys_write(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
 	  break;
-//	case SYS_read:
-//	  sys_read(r):
-//	  break;
-//	case SYS_lseek:
-//	  sys_lseek(r);
-//	  break;
-//	case SYS_close:
-//	  sys_close(r);
-//	  break;
+	case SYS_read:
+	  eax = sys_read(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_lseek:
+	  eax = sys_lseek(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_close:
+	  eax = sys_close(SYSCALL_ARG2(r));
+	  break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   SYSCALL_ARG1(r) = eax;
  
-  return NULL;
+  return r;
 }
