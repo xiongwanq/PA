3dadf61ed4c4b545428cc7478db6033fc068c2e6
tracer-ics2017 <tracer@njuics.org>
2020-06-22 20:51:41 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  20:51:41 up  6:58,  1 user,  load average: 0.00, 0.00, 0.00 afe3466e5265c66d7872c1039ea1ae3dff3681fb

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -33,7 +33,8 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if(cpu.cr0.paging) {
-    if (((addr & 0x1111) + len) > 0x1000) {
+//    if (((addr & 0x1111) + len) > 0x1000) {
+	if((((addr<<20)>>20)+len)>0x1000){
         /* this is a special case, you can handle it later. */
         assert(0);
     }
