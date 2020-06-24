345427dd215856694f112d65de8b157ea0961b8c
tracer-ics2017 <tracer@njuics.org>
2020-06-24 20:57:43 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  20:57:43 up 13 min,  1 user,  load average: 0.17, 0.13, 0.08 3177063f13a2bf777dee351bca751b848b95555a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -84,31 +84,32 @@ paddr_t page_translate(vaddr_t vaddr, bool writing){
   PDE pde;
   PTE pte;
 
-  uint32_t DIR = vaddr >> 22;
-  uint32_t PAGE = (vaddr >> 12) & 0x3ff;
-  uint32_t OFFSET = vaddr & 0xfff;
+  uint32_t Dir = vaddr >> 22;
+//  uint32_t OFFSET = vaddr & 0xfff;
 //  Log("DIR=0x%x,PAGE=0x%x,OFFSET=0x%x\n",DIR,PAGE,OFFSET);
 
-  uint32_t pdaddr = (cpu.cr3.page_directory_base << 12) + (DIR << 2);
+  uint32_t pdaddr = (cpu.cr3.page_directory_base << 12) + (Dir << 2);
 //  Log("pdaddr=0x%x\n",pdaddr);
   pde.val = paddr_read(pdaddr, 4);
 //  Log("pde.val=0x%x\n",pde.val);
   assert(pde.present);
 
+  uint32_t PAGE = (vaddr >> 12) & 0x3ff;
   uint32_t ptaddr = (pde.val & 0xfffff000) + (PAGE << 2);
 //  Log("ptaddr=0x%x\n",ptaddr);
   pte.val = paddr_read(ptaddr, 4);
 //  Log("pte.val=0x%x\n",pte.val);
   assert(pte.present);
 
-  uint32_t paddr = (pte.val & 0xfffff000) + OFFSET;
+//  uint32_t paddr = (pte.val & 0xfffff000) + OFFSET;
+  uint32_t paddr = (pte.val & 0xfffff000) + (vaddr & 0xfff);
 //  Log("paddr=0x%x\n",paddr);
 
 //  if(pde.accessed == 0){
     pde.accessed = 1;
     paddr_write(pdaddr, 4, pde.val);
 //  }
-  if(pte.accessed == 0 || ((pte.dirty == 0) && writing)){
+  if(pte.accessed == 0 || (pte.dirty == 0 && writing)){
     pte.accessed = 1;
     pte.dirty = 1;
   }
