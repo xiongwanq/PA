d8e8d36ca263150ec0f26350aea4264921bed8aa
tracer-ics2017 <tracer@njuics.org>
2020-06-25 21:26:23 +0800

    >  run 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:26:23 up  4:10,  1 user,  load average: 0.05, 0.03, 0.11 85fb44c6f8de15213fe3c4245f1206b5fd31fbfc

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -3,7 +3,7 @@
 
 /* Uncomment these macros to enable corresponding functionality. */
 #define HAS_ASYE
-//#define HAS_PTE
+#define HAS_PTE
 
 void init_mm(void);
 void init_ramdisk(void);
@@ -31,9 +31,9 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/pal");
-  ((void (*)(void))entry)();
-//  load_prog("/bin/pal");
+//  uint32_t entry = loader(NULL, "/bin/pal");
+//  ((void (*)(void))entry)();
+  load_prog("/bin/pal");
 
   panic("Should not reach here");
 }
