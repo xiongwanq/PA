f6d8c8de770fe920089365d785e48d0a6e59ed31
tracer-ics2017 <tracer@njuics.org>
2020-06-26 23:23:26 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  23:23:26 up 18 min,  1 user,  load average: 0.63, 0.29, 0.13 b835756d7d5436ea2541eaf0d7ff464ff59fff41

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,59 +1,142 @@
+//#include "cpu/exec.h"
+//
+//void diff_test_skip_qemu();
+//void diff_test_skip_nemu();
+//
+//make_EHelper(lidt) {
+//  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
+//  if(decoding.is_operand_size_16){
+//	cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
+//  }
+//  else{
+//	cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
+//  }
+//
+//  print_asm_template1(lidt);
+//}
+//
+//make_EHelper(mov_r2cr) {
+//  switch(id_dest->reg){
+//	case 0: cpu.cr0.val = id_src->val;
+//		break;
+//	case 3: cpu.cr3.val = id_src->val;
+//		break;
+//	default: assert(0);
+//		break;
+////  if(id_dest->reg == 0){
+////	cpu.cr0.val = id_src->val;
+////  }
+////  else if(id_dest->reg == 3){
+////	cpu.cr3.val = id_src->val;
+////  }
+////  else{
+////	assert(0);
+//  }
+//
+//  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+//}
+//
+//make_EHelper(mov_cr2r) {
+//  switch(id_dest->reg){
+//	case 0: id_src->val = cpu.cr0.val;
+//		 break;
+//	case 3: id_src->val = cpu.cr3.val;
+//		break;
+//	default: assert(0);
+//		break;
+////  if(id_dest->reg == 0){
+////	id_src->val = cpu.cr0.val;
+////  }
+////  else if(id_dest->reg == 3){
+////	id_src->val = cpu.cr3.val;
+////  }
+////  else{
+////	assert(0);
+//  }
+//
+//  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
+//
+//#ifdef DIFF_TEST
+//  diff_test_skip_qemu();
+//#endif
+//}
+//
+//make_EHelper(int) {
+//  raise_intr(id_dest->val, decoding.seq_eip);
+//
+//  print_asm("int %s", id_dest->str);
+//
+//#ifdef DIFF_TEST
+//  diff_test_skip_nemu();
+//#endif
+//}
+//
+//make_EHelper(iret) {
+//  rtl_pop(&decoding.jmp_eip);
+//  rtl_pop(&cpu.cs);
+//  rtl_pop(&cpu.eflags.value);
+//  decoding.is_jmp = 1;
+//
+//  print_asm("iret");
+//}
+//
+//uint32_t pio_read(ioaddr_t, int);
+//void pio_write(ioaddr_t, int, uint32_t);
+//
+//make_EHelper(in) {
+//  uint32_t t = pio_read(id_src->val, id_dest->width);
+//  operand_write(id_dest, &t);   
+//
+//  print_asm_template2(in);
+//
+//#ifdef DIFF_TEST
+//  diff_test_skip_qemu();
+//#endif
+//}
+//
+//make_EHelper(out) {
+//  pio_write(id_dest->val, id_dest->width, id_src->val);
+//  print_asm_template2(out);
+//
+//#ifdef DIFF_TEST
+//  diff_test_skip_qemu();
+//#endif
+//}
 #include "cpu/exec.h"
 
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
-  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
   if(decoding.is_operand_size_16){
-	cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
+    cpu.idtr.limit = vaddr_read(id_dest->addr,2);//读取2字节
+    cpu.idtr.base = vaddr_read(id_dest->addr+2,3);//顺序读取3字节
   }
   else{
-	cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
+    cpu.idtr.limit = vaddr_read(id_dest->addr,2);//读取2字节
+    cpu.idtr.base = vaddr_read(id_dest->addr+2,4);//顺序往后读取4字节
   }
 
   print_asm_template1(lidt);
 }
 
 make_EHelper(mov_r2cr) {
-  switch(id_dest->reg){
-	case 0: cpu.cr0.val = id_src->val;
-		break;
-	case 3: cpu.cr3.val = id_src->val;
-		break;
-	default: assert(0);
-		break;
-//  if(id_dest->reg == 0){
-//	cpu.cr0.val = id_src->val;
-//  }
-//  else if(id_dest->reg == 3){
-//	cpu.cr3.val = id_src->val;
-//  }
-//  else{
-//	assert(0);
-  }
-
+  if(id_dest->reg == 0)
+    cpu.cr0.val = id_src->val;
+  else if(id_dest->reg == 3)
+    cpu.cr3.val = id_src->val;
+  else
+    assert(0);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  switch(id_dest->reg){
-	case 0: id_src->val = cpu.cr0.val;
-		 break;
-	case 3: id_src->val = cpu.cr3.val;
-		break;
-	default: assert(0);
-		break;
-//  if(id_dest->reg == 0){
-//	id_src->val = cpu.cr0.val;
-//  }
-//  else if(id_dest->reg == 3){
-//	id_src->val = cpu.cr3.val;
-//  }
-//  else{
-//	assert(0);
-  }
-
+  if(id_dest->reg == 0)
+    id_src->val = cpu.cr0.val;
+  else if(id_dest->reg == 3)
+    id_src->val = cpu.cr3.val;
+  else
+    assert(0);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
@@ -62,7 +145,7 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  raise_intr(id_dest->val, decoding.seq_eip);
+  raise_intr(id_dest->val,decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
@@ -72,11 +155,11 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
+  //取出保存现场时保存的数据
   rtl_pop(&decoding.jmp_eip);
-  rtl_pop(&cpu.cs);
+  rtl_pop(&cpu.cs); 
   rtl_pop(&cpu.eflags.value);
-  decoding.is_jmp = 1;
-
+  decoding.is_jmp = 1;//设置跳转
   print_asm("iret");
 }
 
@@ -84,8 +167,8 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  uint32_t t = pio_read(id_src->val, id_dest->width);
-  operand_write(id_dest, &t);   
+  t0 = pio_read(id_src->val,id_dest->width);
+  operand_write(id_dest,&t0);
 
   print_asm_template2(in);
 
@@ -95,10 +178,13 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  pio_write(id_dest->val, id_dest->width, id_src->val);
+  rtl_li(&t0,id_dest->val);
+  pio_write(t0,id_dest->width,id_src->val);
+
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
 #endif
 }
+
