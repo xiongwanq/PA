a5c2ed0c0cba0eae9c9a2392f80dbbee8ee9e671
tracer-ics2017 <tracer@njuics.org>
2020-06-26 23:37:51 +0800

    >  compile 161830301 xiongwanqing Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  23:37:51 up 32 min,  1 user,  load average: 0.42, 0.30, 0.20 7265ad993b6c32e1b862cdb36ce81206d19e39a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,134 +1,47 @@
-//#include "common.h"
-//#include "syscall.h"
-//#include "fs.h"
-////#include "memory.h"
-//
-//int mm_brk(uint32_t new_brk);
-//
-//static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-//  return fs_open((char *)pathname, flags, mode);
-//}
-//
-//static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-////  Log();
-//  return fs_write(fd, (void *)buf, len);
-//}
-//
-//static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-//  return fs_read(fd, (void *)buf, len);
-//}
-//
-//static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
-//  return fs_lseek(fd, offset, whence);
-//}
-//
-//static inline uintptr_t sys_close(uintptr_t fd) {
-//  return fs_close(fd);
-//}
-//
-//static inline uintptr_t sys_brk(_RegSet *r) {
-////  Log("sys_brk\n");
-//  SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
-//  Log("eax=0x%x,ebx=0x%x,mm_brk(ebx)=0x%x\n",SYSCALL_ARG1(r),SYSCALL_ARG2(r),mm_brk(SYSCALL_ARG2(r)));
-//  return 0;
-//}
-//
-//static inline uintptr_t sys_none() {
-////  SYSCALL_ARG1(r) = 1;
-//  return 1;
-//}
-//  
-//static inline uintptr_t sys_exit(_RegSet *r) {
-//  _halt(SYSCALL_ARG2(r));
-//  return 1;
-//}
-//
-//_RegSet* do_syscall(_RegSet *r) {
-//  uintptr_t a[4];
-//  a[0] = SYSCALL_ARG1(r);
-//  a[1] = SYSCALL_ARG2(r);
-//  a[2] = SYSCALL_ARG3(r);
-//  a[3] = SYSCALL_ARG4(r);
-//  switch (a[0]) {
-//    case SYS_none:
-//	  SYSCALL_ARG1(r) = sys_none();
-//	  break;
-//    case SYS_exit:
-//	  sys_exit(r);
-//	  break;
-//	case SYS_brk:
-//	  sys_brk(r);
-//	  break;
-//	case SYS_open:
-//	  SYSCALL_ARG1(r) = sys_open(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
-//	  break;
-//	case SYS_write:
-////	  Log("use write\n");
-//	  SYSCALL_ARG1(r) = sys_write(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
-//	  break;
-//	case SYS_read:
-//	  SYSCALL_ARG1(r) = sys_read(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
-//	  break;
-//	case SYS_lseek:
-//	  SYSCALL_ARG1(r) = sys_lseek(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
-//	  break;
-//	case SYS_close:
-//	  SYSCALL_ARG1(r) = sys_close(SYSCALL_ARG2(r));
-//	  break;
-//
-//    default: panic("Unhandled syscall ID = %d", a[0]);
-//  }
-//
-//  return r;
-////  return NULL;
-//}
 #include "common.h"
 #include "syscall.h"
 #include "fs.h"
+//#include "memory.h"
+
 int mm_brk(uint32_t new_brk);
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  //Log("run sys_open\n");
-  return fs_open((char*)pathname,flags,mode);
+  return fs_open((char *)pathname, flags, mode);
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  //Log("run sys_write\n");
-  return fs_write(fd,(void *)buf,len);//对buf强制类型转换
+//  Log();
+  return fs_write(fd, (void *)buf, len);
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  //Log("run sys_read\n");
-  return fs_read(fd,(void *)buf,len);
+  return fs_read(fd, (void *)buf, len);
 }
 
 static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
-  //Log("run sys_lseek\n");
   return fs_lseek(fd, offset, whence);
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-  //Log("run sys_close\n");
   return fs_close(fd);
 }
 
 static inline uintptr_t sys_brk(_RegSet *r) {
+//  Log("sys_brk\n");
   SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
+  Log("eax=0x%x,ebx=0x%x,mm_brk(ebx)=0x%x\n",SYSCALL_ARG1(r),SYSCALL_ARG2(r),mm_brk(SYSCALL_ARG2(r)));
   return 0;
 }
 
-static inline uintptr_t sys_none(_RegSet *r) {
-  //Log("run sys_none\n");
-  SYSCALL_ARG1(r) = 1;
+static inline uintptr_t sys_none() {
+//  SYSCALL_ARG1(r) = 1;
   return 1;
 }
-
+  
 static inline uintptr_t sys_exit(_RegSet *r) {
-  //Log("run sys_exit\n");
   _halt(SYSCALL_ARG2(r));
   return 1;
 }
-
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
@@ -136,35 +49,36 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
- 
   switch (a[0]) {
     case SYS_none:
-      sys_none(r);
-      break;
+	  SYSCALL_ARG1(r) = sys_none();
+	  break;
     case SYS_exit:
-      sys_exit(r);
-      break;
-    case SYS_write:
-      SYSCALL_ARG1(r) = sys_write(a[1],a[2],a[3]);
-      break;
-    case SYS_brk:
-      sys_brk(r);
-      break;
-    case SYS_close:
-      SYSCALL_ARG1(r) = sys_close(a[1]);
-      break;
-    case SYS_read:
-      SYSCALL_ARG1(r) = sys_read(a[1],a[2],a[3]);
-      break;
-    case SYS_open:
-      SYSCALL_ARG1(r) = sys_open(a[1],a[2],a[3]);
-      break;
-    case SYS_lseek:
-      SYSCALL_ARG1(r) = sys_lseek(a[1],a[2],a[3]);
-      break;
+	  sys_exit(r);
+	  break;
+	case SYS_brk:
+	  sys_brk(r);
+	  break;
+	case SYS_open:
+	  SYSCALL_ARG1(r) = sys_open(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_write:
+//	  Log("use write\n");
+	  SYSCALL_ARG1(r) = sys_write(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_read:
+	  SYSCALL_ARG1(r) = sys_read(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_lseek:
+	  SYSCALL_ARG1(r) = sys_lseek(SYSCALL_ARG2(r), SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+	  break;
+	case SYS_close:
+	  SYSCALL_ARG1(r) = sys_close(SYSCALL_ARG2(r));
+	  break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  return NULL;
+  return r;
+//  return NULL;
 }
-
