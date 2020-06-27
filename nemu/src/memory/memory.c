c41c4911eac865b80b4e714c456b5468d5c5d3a8
161830301-Xiong Wanqing <1193611524@qq.com>
2020-06-27 12:46:19 +0800

    PA3.2 //# Please enter the commit message for your changes. Lines starting //# with '#' will be ignored, and an empty message aborts the commit. //# //# On branch pa3 //# Your branch is ahead of 'myrepo3.2/pa3' by 41 commits. //#   (use "git push" to publish your local commits) //#

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -33,8 +33,7 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if(cpu.cr0.paging) {
-//    if (((addr & 0xfff) + len) > 0x1000) {
-	if((((addr<<20)>>20)+len)>0x1000){
+    if (((addr & 0xfff) + len) > 0x1000) {
 		int firSize,secSize;
         firSize = 0x1000 - (addr & 0xfff);
         secSize = len - firSize; 
@@ -58,8 +57,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   if(cpu.cr0.paging) {
-//    if (((addr & 0xfff) + len) > 0x1000) {
-	if((((addr<<20)>>20)+len)>0x1000){
+    if (((addr & 0xfff) + len) > 0x1000) {
 		int firSize,secSize;
         firSize = 0x1000 - (addr & 0xfff);
         secSize = len - firSize; 
@@ -84,31 +82,30 @@ paddr_t page_translate(vaddr_t vaddr, bool writing){
   PDE pde;
   PTE pte;
 
-  uint32_t Dir = vaddr >> 22;
-//  uint32_t OFFSET = vaddr & 0xfff;
+  uint32_t DIR = vaddr >> 22;
+  uint32_t PAGE = (vaddr >> 12) & 0x3ff;
+  uint32_t OFFSET = vaddr & 0xfff;
 //  Log("DIR=0x%x,PAGE=0x%x,OFFSET=0x%x\n",DIR,PAGE,OFFSET);
 
-  uint32_t pdaddr = (cpu.cr3.page_directory_base << 12) + (Dir << 2);
+  uint32_t pdaddr = (cpu.cr3.page_directory_base << 12) + (DIR << 2);
 //  Log("pdaddr=0x%x\n",pdaddr);
   pde.val = paddr_read(pdaddr, 4);
 //  Log("pde.val=0x%x\n",pde.val);
   assert(pde.present);
 
-  uint32_t PAGE = (vaddr >> 12) & 0x3ff;
   uint32_t ptaddr = (pde.val & 0xfffff000) + (PAGE << 2);
 //  Log("ptaddr=0x%x\n",ptaddr);
   pte.val = paddr_read(ptaddr, 4);
 //  Log("pte.val=0x%x\n",pte.val);
   assert(pte.present);
 
-//  uint32_t paddr = (pte.val & 0xfffff000) + OFFSET;
-  uint32_t paddr = (pte.val & 0xfffff000) + (vaddr & 0xfff);
+  uint32_t paddr = (pte.val & 0xfffff000) + OFFSET;
 //  Log("paddr=0x%x\n",paddr);
 
-//  if(pde.accessed == 0){
+  if(pde.accessed == 0){
     pde.accessed = 1;
     paddr_write(pdaddr, 4, pde.val);
-//  }
+  }
   if(pte.accessed == 0 || (pte.dirty == 0 && writing)){
     pte.accessed = 1;
     pte.dirty = 1;
